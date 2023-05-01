ef78b8a65f8ff114d1bf42bd021b59ae83a19cde
Admin <Admin@DESKTOP-26OQHDE>
2023-05-01 08:05:41 +0530

    comitt and push

--- src/test/java/practice/Sample.java
+++ src/test/java/practice/Sample.java
@@ -3,6 +3,7 @@ package practice;
 public class Sample {
 	public static void main(String[] args) {
 		System.out.println("sample done by framework developer");
+		System.out.println("bye bye");
 	}
 
 }
